67a57f28ac8968005d23c5affa268d13a55b4233
dlawjddn <jwl5015@naver.com>
2023-11-23 01:30:40 +0900

    Feat: gpt api 설정값 필드 추가

--- GPT-Counselor/src/main/java/project/chatbot/GPTCounselor/dto/gpt/request/GptRequest.java
+++ GPT-Counselor/src/main/java/project/chatbot/GPTCounselor/dto/gpt/request/GptRequest.java
@@ -9,8 +9,11 @@ import java.util.List;
 public class GptRequest {
     private final String model = "gpt-3.5-turbo";
     private List<Message> messages;
+    private final int max_tokens = 256;
+    private double temperature;
     @Builder
-    public GptRequest(List<Message> messages) {
+    public GptRequest(List<Message> messages, double temperature) {
         this.messages = messages;
+        this.temperature = temperature;
     }
 }
