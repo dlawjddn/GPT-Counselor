8b3bd9e1c65c626bbb17ef258d2aebc32332c9fe
dlawjddn <jwl5015@naver.com>
2023-11-23 01:31:33 +0900

    Feat: gpt api response dto 생성

---
+++ GPT-Counselor/src/main/java/project/chatbot/GPTCounselor/dto/chat/response/GptChatDTO.java
@@ -0,0 +1,10 @@
+package project.chatbot.GPTCounselor.dto.chat.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GptChatDTO {
+    private String gptChat;
+}
