34f2e3bd37f4bd4aab913cd974f1dc6591bc9ab9
dlawjddn <jwl5015@naver.com>
2023-11-22 16:49:54 +0900

    Feat: member constructor 생성

--- GPT-Counselor/src/main/java/project/chatbot/GPTCounselor/domain/Member.java
+++ GPT-Counselor/src/main/java/project/chatbot/GPTCounselor/domain/Member.java
@@ -1,6 +1,7 @@
 package project.chatbot.GPTCounselor.domain;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -19,4 +20,9 @@ public class Member {
     private String password;
     @OneToMany(mappedBy = "member")
     private List<Consulting> consultings = new ArrayList<>();
+    @Builder
+    public Member(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
 }
