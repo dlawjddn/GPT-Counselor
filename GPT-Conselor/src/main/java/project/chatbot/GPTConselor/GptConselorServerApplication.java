fab7f36eb76b2ca65b62f65777e085271628e36d
dlawjddn <jwl5015@naver.com>
2023-11-22 15:14:20 +0900

    first commit

---
+++ GPT-Conselor/src/main/java/project/chatbot/GPTConselor/GptConselorServerApplication.java
@@ -0,0 +1,13 @@
+package project.chatbot.GPTConselor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GptConselorServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GptConselorServerApplication.class, args);
+	}
+
+}
