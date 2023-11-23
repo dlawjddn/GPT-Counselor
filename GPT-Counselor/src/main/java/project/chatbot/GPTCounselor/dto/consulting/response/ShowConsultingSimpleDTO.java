df6604e9eff47f9c3c85e33b9d52c30f3062cf31
dlawjddn <jwl5015@naver.com>
2023-11-23 16:20:12 +0900

    Feat: 상담 별 채팅 기록 제공 dto 생성

---
+++ GPT-Counselor/src/main/java/project/chatbot/GPTCounselor/dto/consulting/response/ShowConsultingSimpleDTO.java
@@ -0,0 +1,11 @@
+package project.chatbot.GPTCounselor.dto.consulting.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ShowConsultingSimpleDTO {
+    private Long consultingId;
+    private String title;
+}
