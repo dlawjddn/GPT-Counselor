97953eab2e4ae6cbbf7920e2633d15c337a4112a
dlawjddn <jwl5015@naver.com>
2023-11-23 16:19:43 +0900

    solution dto 생성

---
+++ GPT-Counselor/src/main/java/project/chatbot/GPTCounselor/dto/consulting/response/SaveSolutionDTO.java
@@ -0,0 +1,10 @@
+package project.chatbot.GPTCounselor.dto.consulting.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SaveSolutionDTO {
+    private String solution;
+}
