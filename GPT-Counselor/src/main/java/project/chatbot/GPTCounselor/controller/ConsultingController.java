ac52c49b8d3c36e7fe9c2a2e1287c5d473e1d341
dlawjddn <jwl5015@naver.com>
2023-11-23 16:21:07 +0900

    Feat: 상담 별 채팅 기록 제공 api 생성

--- GPT-Counselor/src/main/java/project/chatbot/GPTCounselor/controller/ConsultingController.java
+++ GPT-Counselor/src/main/java/project/chatbot/GPTCounselor/controller/ConsultingController.java
@@ -1,12 +1,16 @@
 package project.chatbot.GPTCounselor.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import project.chatbot.GPTCounselor.dto.chat.response.ConsultingChatDTO;
 import project.chatbot.GPTCounselor.dto.consulting.request.SaveConsultingDTO;
+import project.chatbot.GPTCounselor.dto.consulting.response.SaveSolutionDTO;
+import project.chatbot.GPTCounselor.dto.consulting.response.ShowConsultingSimpleDTO;
 import project.chatbot.GPTCounselor.service.ConsultingService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -16,5 +20,17 @@ public class ConsultingController {
     @PostMapping("/users/consulting")
     public void saveConsulting(@RequestBody SaveConsultingDTO saveConsultingDTO){
         consultingService.saveConsulting(saveConsultingDTO);
+    }
+    @GetMapping("/users/consulting")
+    public List<ShowConsultingSimpleDTO> showSimpleConsults(){
+        return consultingService.showSimpleConsults();
+    }
+    @GetMapping("/users/consulting/")
+    public ConsultingChatDTO showChatsByConsulting(@RequestParam Long id){
+        return consultingService.showChatsByConsulting(id);
+    }
+    @GetMapping("/users/consulting/solution")
+    public SaveSolutionDTO makeSolution(@RequestParam Long id) throws JsonProcessingException {
+        return consultingService.makeSolution(id);
     }
 }
