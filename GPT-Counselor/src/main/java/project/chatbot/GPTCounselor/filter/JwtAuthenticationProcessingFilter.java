37e1b0666c81be760956be9dafc5c0f53f5ffb78
dlawjddn <jwl5015@naver.com>
2023-11-25 20:00:03 +0900

    Feat: jwt filter 생성

--- GPT-Counselor/src/main/java/project/chatbot/GPTCounselor/filter/JwtAuthenticationProcessingFilter.java
+++ GPT-Counselor/src/main/java/project/chatbot/GPTCounselor/filter/JwtAuthenticationProcessingFilter.java
@@ -4,13 +4,27 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import project.chatbot.GPTCounselor.provider.TokenProvider;
 
 import java.io.IOException;
-
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
+    private final TokenProvider tokenProvider;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
+        String token = tokenProvider.resolveToken(request);
+        if (token != null && tokenProvider.isValidateToken(token)){
+            Authentication authentication = tokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
     }
 }
