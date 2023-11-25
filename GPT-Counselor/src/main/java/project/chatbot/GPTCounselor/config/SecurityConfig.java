cb59b0c65745ae519e593a245817ee40c2c070c7
dlawjddn <jwl5015@naver.com>
2023-11-25 20:00:19 +0900

    Feat: Spring Security config 생성

--- GPT-Counselor/src/main/java/project/chatbot/GPTCounselor/config/SecurityConfig.java
+++ GPT-Counselor/src/main/java/project/chatbot/GPTCounselor/config/SecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import project.chatbot.GPTCounselor.filter.JwtAuthenticationProcessingFilter;
 import project.chatbot.GPTCounselor.handler.CustomAccessDeniedHandler;
 import project.chatbot.GPTCounselor.handler.CustomAuthenticationEntryPoint;
 
@@ -20,6 +22,7 @@ import project.chatbot.GPTCounselor.handler.CustomAuthenticationEntryPoint;
 public class SecurityConfig {
     private final CustomAuthenticationEntryPoint authenticationEntryPoint;
     private final CustomAccessDeniedHandler accessDeniedHandler;
+    private final JwtAuthenticationProcessingFilter jwtAuthenticationProcessingFilter;
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
@@ -42,7 +45,8 @@ public class SecurityConfig {
                 .exceptionHandling(exception ->exception
                         .authenticationEntryPoint(authenticationEntryPoint)
                         .accessDeniedHandler(accessDeniedHandler)
-                );
+                )
+                .addFilterBefore(jwtAuthenticationProcessingFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
